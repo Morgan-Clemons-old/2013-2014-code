17bcc2356c01fadc2099bee73fdab447302b17df
Morgan Clemons <moranator97@aol.com>
2013-09-30 09:53:00 -0400

    Update NewFile_Template004.c
    fixed and cleaned file

--- NewFile_Template004.c
+++ NewFile_Template004.c
@@ -1,12 +1,9 @@
-##define definitions_H_
-ifdef definitions_H_
+
+#ifdef definitions_H_
+#define definitions_H_
 #define MOTOR_OFF           0
 #define MOTOR_HALF          50
-#define MOTOR_FULL          100
-
-
-
-
+#define MOTOR_FULL          100 
 
 
 #endif definitions_H_
