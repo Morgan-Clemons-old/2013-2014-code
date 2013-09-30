0aeb895ced0fa0262346fe1408db9d87390ff5e9
Morgan Clemons <moranator97@aol.com>
2013-09-30 10:00:26 -0400

    second commit with changes
    renamed and deleted files in repo.

--- NewFile_Template004.c
+++
@@ -1,9 +0,0 @@
-
-#ifdef definitions_H_
-#define definitions_H_
-#define MOTOR_OFF           0
-#define MOTOR_HALF          50
-#define MOTOR_FULL          100 
-
-
-#endif definitions_H_
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-2013-2014-robot
+2013-2014-code
 ===============
+this is a repo containing all of Morgans code for the 2013-2014 season(good & bad)
